94f19214073e7407220abe574dd8a19532bd443b
Jeffrey <jeff.1001@live.com>
2022-11-08 16:13:57 -0600

    navbar and routing tweaks

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,14 @@
 //react features
 import React, {useState, useEffect} from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 
 //styling
 import "../navbar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import logo from '../images/bluesaber.png' 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav'
 
 export default function Navbar(){
  const [toggleMenu, setToggleMenu] = useState(false)
@@ -29,14 +31,23 @@ useEffect(() => {
  
  return(
   <nav>
+   <Container>
    {(toggleMenu || screenWidth > 500) && (
-   <ul className="list">
-   <li className="items">Home</li>
-   <li className="items">About</li>
-   <li className="items">Search</li> 
- </ul>
+   <Nav className="list">
+    <Nav.Item className="items">
+     <Link className='link' to='/'>
+      <img src={logo}/>
+     </Link>
+    </Nav.Item>
+    <Nav.Item className="items">
+     <Link className='link' to='/about'>
+      About
+     </Link>
+    </Nav.Item>
+ </Nav>
    )}
    <FontAwesomeIcon onClick={toggleNav} className="btn" icon={faBars} />
+   </Container>
   </nav>
  )
 }
