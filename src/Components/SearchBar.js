8ca83544e1a8f8b41e38584287d8d1691c02d8f5
Jeffrey <jeff.1001@live.com>
2022-11-12 12:15:21 -0600

    searchbar component created

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,18 @@
+//react features
+import React, {useState, useEffect} from 'react'
+
+
+//styling
+// import "../SearchBar.css"
+
+
+
+export default function SearchBar(){
+
+ return(
+  <div className="searchbar">
+   <h1>HELLO WORLD</h1>
+
+  </div>
+ )
+}
