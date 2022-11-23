1222072562afae4d197382d74bb232e781868760
Jeffrey <jeff.1001@live.com>
2022-11-23 17:49:16 -0600

    search works with mock data

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -4,6 +4,7 @@ import React, {useState, useEffect} from 'react'
 
 //styling
 import "../Home.css"
+import SearchBar from './SearchBar'
 
 
 
@@ -12,6 +13,7 @@ export default function HomePage(){
  return(
   <div className="homepage">
    <h1>HELLO WORLD</h1>
+   <SearchBar />
 
   </div>
  )
